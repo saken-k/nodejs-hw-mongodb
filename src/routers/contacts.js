3fe46df91174927007ff817e9ec9d46c99de97af
saken-k <skar.ksk@gmail.com>
2025-04-05 00:20:44 +0300

    rest api

---
+++ src/routers/contacts.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+import {
+  createContactController,
+  deleteContactController,
+  getContactByIdController,
+  getContactsController,
+  patchContactController,
+} from '../controllers/contacts.js';
+
+const router = Router();
+
+router.get('/', ctrlWrapper(getContactsController));
+
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/', ctrlWrapper(createContactController));
+
+router.patch('/:contactId', ctrlWrapper(patchContactController));
+
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import cors from 'cors';
 import pino from 'pino-http';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+import router from './routers/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -21,44 +24,11 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Server is starting successfully',
-    });
-  });
+  app.use('/contacts', router);
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
