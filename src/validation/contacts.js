27b3b2df77c395c7443f503e138b79b7ad83e115
saken-k <skar.ksk@gmail.com>
2025-04-12 14:27:57 +0300

    update validation schema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -14,7 +14,7 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.STRING().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
